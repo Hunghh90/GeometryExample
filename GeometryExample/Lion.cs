f1a69fa65eb7ac0e48417c84318b23655b548e94
Hunghh90 <hungbkvn9x1@gmail.com>
2023-01-10 20:01:24 +0700

    up

--- GeometryExample/Lion.cs
+++ GeometryExample/Lion.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GeometryExample
+namespace GeometryExample.Exercise2
 {
     internal class Lion : Animal
     {
--- GeometryExample/Tiger.cs
+++ GeometryExample/Tiger.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GeometryExample
+namespace GeometryExample.Exercise2
 {
     public class Tiger : Animal
     {
