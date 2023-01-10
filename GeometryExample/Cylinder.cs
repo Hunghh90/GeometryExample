3f50aa7eb87e5ffcfddcdae08175c1ac777c9ad6
Hunghh90 <hungbkvn9x1@gmail.com>
2023-01-10 17:32:47 +0700

    Add project files.

---
+++ GeometryExample/Cylinder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometryExample
+{
+    public class Cylinder
+    {
+        public void Process()
+        {
+
+        }
+
+        public void Result()
+        {
+
+        }
+    }
+}
