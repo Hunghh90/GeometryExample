e172605365f5cb12c6f3fc777533be1e6754fcfc
Hunghh90 <hungbkvn9x1@gmail.com>
2023-01-10 17:39:18 +0700

    up

--- GeometryExample/Cylinder.cs
+++ GeometryExample/Cylinder.cs
@@ -8,8 +8,20 @@ namespace GeometryExample
 {
     public class Cylinder
     {
+        public Cylinder() { }
+
+        public float Radius { get; set; }
+
+        public float Height { get; set; }  
         public void Process()
         {
+            float BaseArea = (float)(Radius * Radius * Math.PI);
+
+            float LateralArea = (float)(2 * Math.PI * Radius * Height);
+
+            float TotalArea = (float)(2 * Math.PI * Radius * (Height + Radius));
+
+            float Volume = (float)(Math.PI * Radius * Radius * Height);
 
         }
 
